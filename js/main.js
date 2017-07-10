126b9db4d3abc02e01bd61e51311b73b18002546
sanouo <sanouo591@gmail.com>
2017-07-10 16:22:19 +0200

    modification of the css

--- js/main.js
+++ js/main.js
@@ -1,21 +1,54 @@
 var choix = document.querySelectorAll("button");
 var utilisateur = prompt("Entrez votre pseudo");
-document.querySelector(".utilisateur").innerHTML = ` ${utilisateur} `;
+
+
+document.querySelector(".utilisateur").innerHTML = utilisateur;
+
  for (let i = 0; i < choix.length; i++){
  choix[i].addEventListener("click", function() {
-   var joueur = choix[i].innerHTML;
-   var ordinateur = choix[Math.floor(Math.random() * choix.length)].innerHTML;
+   var joueur = choix[i].className;
+   var boutonOrdinateur = choix[Math.floor(Math.random() * choix.length)];
    var resultat;
-     if (joueur === ordinateur)
-       resultat = "Vous avez fait une egalité !";
-     else if ((joueur === "pierre" && ordinateur === "ciseaux") || (joueur === "feuille" && ordinateur === "pierre") || (joueur === "ciseaux" && ordinateur === "feuille"))
-       resultat = "Vous avez gagné !";
-    else
-       resultat = "Vous avez perdu !";
+var ordinateur =  boutonOrdinateur.firstElementChild.alt;
+var image =  boutonOrdinateur.innerHTML;
+
+var resultatjoueur=0;
+var resultatordi=0;
+
+       if (joueur === ordinateur){
+           resultat = "egalite";
+       }
+         else if (joueur === "ciseau"){
+          if (ordinateur === "pierre"){
+         resultatordi++; resultat = "Ordinateur a gagne";
+          }
+           else {
+         resultatjoueur++; resultat = "joueur a gagne";
+           }
+         }
+
+         else if (joueur === "feuille"){
+          if (ordinateur === "pierre"){
+          resultatjoueur++; resultat = "joueur a gagne";
+          }
+           else {
+          resultatordi++; resultat = "Ordinateur a gagne";
+           }
+         }
+
+         else if (joueur === "pierre"){
+          if (ordinateur === "ciseau"){
+          resultatjoueur++; resultat = "joueur a gagne";
+          }
+           else {
+          resultatordi++;  resultat = "Ordinateur a gagne";
+           }
+       }
+
 document.querySelector(".resultat").innerHTML = `
- ${utilisateur} : ${joueur}
- Ordinateur a choisi : ${ordinateur}
- ${resultat}
- `;
+${image}
+${resultat}
+${image}
+`;
 })
 }
