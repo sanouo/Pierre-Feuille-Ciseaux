bbf457ab76efe7f855b3b29a6dfd7a9a61579999
sanouo <sanouo591@gmail.com>
2018-03-19 08:35:17 +0100

    modification

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ var utilisateur = prompt("Entrez votre pseudo");
 
 var resultatjoueur= 0;
 var resultatordi= 0;
+var resultattotal=0;
 
+
+var personnage = document.getElementById("personnage");
+var personnage1 = document.getElementById("personnage1");
 
 
 
@@ -19,6 +23,8 @@ var ordinateur =  boutonOrdinateur.firstElementChild.alt;
 var image =  boutonOrdinateur.innerHTML;
 var tof = choix[i].innerHTML;
 
+
+
 function jeux (){
 
 
@@ -27,44 +33,98 @@ function jeux (){
        }
          else if (joueur === "ciseaux"){
           if (ordinateur === "pierre"){
-         resultatordi++; resultat = "Ordinateur a gagne";
+         resultatordi++; resultat = "Ordinateur a gagne";resultattotal++;
           }
            else {
-         resultatjoueur++; resultat = "Joueur a gagne";
+         resultatjoueur++; resultat = "Joueur a gagne";resultattotal++;
            }
          }
 
          else if (joueur === "feuille"){
           if (ordinateur === "pierre"){
-          resultatjoueur++; resultat = "Joueur a gagne";
+          resultatjoueur++; resultat = "Joueur a gagne";resultattotal++;
           }
            else {
-          resultatordi++; resultat = "Ordinateur a gagne";
+          resultatordi++; resultat = "Ordinateur a gagne";resultattotal++;
            }
          }
 
          else if (joueur === "pierre"){
           if (ordinateur === "ciseaux"){
-          resultatjoueur++; resultat = "Joueur a gagne";
+          resultatjoueur++; resultat = "Joueur a gagne";resultattotal++;
           }
            else {
-          resultatordi++;  resultat = "Ordinateur a gagne";
+          resultatordi++;  resultat = "Ordinateur a gagne";resultattotal++;
            }
        }
 
 }
 
-jeux();
+
+if (resultatjoueur === 3 || resultatordi === 3) {
+
+  alert("Fin du game !");
+
+if (resultatjoueur === 3){
+  alert("Vous avez gagner");
+}
+else {
+  alert("Vous avez perdu");
+}
+
+location.reload();
+
+}
+
+else{
+  jeux();
+}
+
+
+if (resultatjoueur === 2 || resultatordi === 2) {
+  document.querySelector(".resultatjoueur").innerHTML = resultatjoueur;
+  document.querySelector(".resultatordi").innerHTML = resultatordi;
+  document.querySelector(".resultat").innerHTML = `
+  ${tof}
+  ${resultat}
+  ${image}
+  `;
 
 
 
- document.querySelector(".resultatjoueur").innerHTML = resultatjoueur;
- document.querySelector(".resultatordi").innerHTML = resultatordi;
+  if (resultattotal === 2 || resultattotal === 3) {
 
-document.querySelector(".resultat").innerHTML = `
-${tof}
-${resultat}
-${image}
-`;
+    alert("Attend je te ramene mon grand frere !");
+    alert("ah c est comme sa ! moi aussi");
+    resultattotal++;
+  }
+
+
+
+if (resultatjoueur >= 2 || resultatordi >= 2) {
+  personnage.src="img/goku.png";
+  personnage1.src="img/naru.png";
+}
+else {
+  personnage.src="img/sangoku.png";
+  personnage1.src="img/naruto.jpeg";
+}
+
+}
+else {
+
+   document.querySelector(".resultatjoueur").innerHTML = resultatjoueur;
+   document.querySelector(".resultatordi").innerHTML = resultatordi;
+
+   document.querySelector(".resultat").innerHTML = `
+   ${tof}
+   ${resultat}
+   ${image}
+   `;
+
+}
+
+
+
 })
 }
